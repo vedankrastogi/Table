2d7fabe8fe6b06441ef37b2f885734a5b8818cd8
vedankrastogi <79106599+vedankrastogi@users.noreply.github.com>
2021-04-08 23:36:49 +0530

    table of a given number

---
+++ src/TableOfGivenNumber.java
@@ -0,0 +1,10 @@
+public class TableOfGivenNumber {
+    public static void main(String[] args) {
+        int a=5;
+        int table;
+        for(int i=1;i<=10;i++){
+            table=a*i;
+            System.out.println(table);
+        }
+    }
+}
